5656c44a47d513d9e3674f76022664003605e5ce
Rutger Muller <rutger@Rutgers-MacBook-Pro.local>
2017-06-21 23:33:40 +0200

    Added a form custom. Dont like crispyforms yet. Very nice this way time to style it!

--- home/forms.py
+++ home/forms.py
@@ -1,9 +1,10 @@
-from django import forms
+from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
 from .models import Leads
 from crispy_forms.helper import FormHelper
 
 
-class Berekening(forms.ModelForm):
+class Berekening(ModelForm):
     """Form: the calculationForm"""
     helper = FormHelper()
 
@@ -21,3 +22,15 @@ class Berekening(forms.ModelForm):
             'phone',
             'bkr'
         ]
+        labels = {
+            'first_name': _('Voornaam'),
+            'last_name': _('Achternaam'),
+            'adress': _('Adres'),
+            'home_number': _('Huisnummer'),
+            'zipcode': _('Postcode'),
+            'income': _('Inkomen'),
+            'income_partner': _('Inkomen partner (optioneel)'),
+            'email': _('Emailadres'),
+            'phone': _('Mobiele telefoon'),
+
+        }
